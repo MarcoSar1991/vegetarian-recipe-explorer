e90e290477237a823b9016f58e53b4870474ec96
msarcina <msarcina@deloitte.it>
2026-01-28 12:09:29 +0100

    main pointer change + adding strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <FavoritesProvider>
-    <App />
-  </FavoritesProvider>
+  <StrictMode>
+    <FavoritesProvider>
+      <App />
+    </FavoritesProvider>
+  </StrictMode>
 )
