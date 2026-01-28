a1652e42979b25ffd3146d28bb8ae1c7306c5625
msarcina <msarcina@deloitte.it>
2026-01-28 12:42:18 +0100

    change app router to work on github page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import RecipeDetails from "./pages/RecipeDetails.jsx";
 import Favorites from "./pages/Favorites.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="min-h-screen bg-gray-50">
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/recipe/:id" element={<RecipeDetails />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
-
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
